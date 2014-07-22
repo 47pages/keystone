b762e6f6edf49cb299775e132f69e41573aa775a
Matt Dahl <matt.dahl.2013@gmail.com>
2014-07-22 01:12:40 -0400

    Adds auth permissions for Meeting and Volume models.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -21,15 +21,12 @@ exports = module.exports = {
 		case 'LiteratureSubmission':
 			switch (field.label) {
 			case 'Title':
+			case 'Status':
+			case 'Staff Meeting Assignment':
+			case 'Publish Online':
 				return user.isSeniorDesign || user.isSeniorLiterature || user.permissionLevel > this.permissionLevels.senior;
 			case 'Edited Piece':
 				return user.isJuniorLiterature || user.permissionLevel > this.permissionLevels.junior;
-			case 'Status':
-				return user.isSeniorDesign || user.isSeniorLiterature || user.permissionLevel > this.permissionLevels.senior;
-			case 'Staff Meeting Assignment':
-				return user.isSeniorDesign || user.isSeniorLiterature || user.permissionLevel > this.permissionLevels.senior;
-			case 'Publish Online':
-				return user.isSeniorDesign || user.isSeniorLiterature || user.permissionLevel > this.permissionLevels.senior;
 			default:
 				return true;
 			}
@@ -37,25 +34,36 @@ exports = module.exports = {
 		case 'ArtSubmission':
 			switch (field.label) {
 			case 'Title':
+			case 'Status':
+			case 'Staff Meeting Assignment':
+			case 'Publish Online':
 				return user.isSeniorDesign || user.isSeniorArt || user.permissionLevel > this.permissionLevels.senior;
 			case 'Edited Image':
 				return user.isJuniorArt || user.permissionLevel > this.permissionLevels.senior;
-			case 'Status':
-				return user.isSeniorDesign || user.isSeniorArt || user.permissionLevel > this.permissionLevels.senior;
-			case 'Staff Meeting Assignment':
-				return user.isSeniorDesign || user.isSeniorArt || user.permissionLevel > this.permissionLevels.senior;
-			case 'Publish Online':
-				return user.isSeniorDesign || user.isSeniorArt || user.permissionLevel > this.permissionLevels.senior;
 			default:
 				return true;
 			}
 			break;
+		case 'Volume':
+			switch (field.label) {
+			case 'Number':
+				return user.isEditor || this.permissionLevels.editor;
+			default:
+				return true;
+			}
+		case 'Meeting':
+			switch (field.label) {
+			case 'Minutes':
+				return user.isSeniorDesign || user.isSeniorLiterature || user.isSeniorArt || user.permissionLevel > this.permissionLevels.senior;
+			default:
+				return true;
+			}
 		default:
 			return true;
 		}
 	},
 	/**
-	 * Performs an authenication check to see if a user can see a field (used for hiding conditional data like author name)
+	 * Performs an authentication check to see if a user can see a field (used for hiding conditional data like author name)
 	 * @param  {String} field
 	 * @param  {Object} user
 	 * @return {Boolean}
@@ -84,13 +92,6 @@ exports = module.exports = {
 			return true;
 		}
 	}
-	/**
-	 * Performs an authentication check to see if a user can perform a given aciton on a model
-	 * @param  {Object} model
-	 * @param  {String} action Valid actions are 'view' and 'edit'
-	 * @param  {Object} user
-	 * @return {Boolean}
-	 */
 };
 
 
